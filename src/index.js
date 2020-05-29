df7654a78713ee1d66043a2690cda203b1cff619
olejos777 <olejniczakk.maciejj@gmail.com>
2020-05-29 16:57:42 +0200

    12.2 add first components

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles/normalize.scss';
+import './styles/global.scss';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
